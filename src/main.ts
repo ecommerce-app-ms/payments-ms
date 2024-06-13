286db5f393f20ff99e72696a232644b05c609aef
Eduardopmx <eduardop.29.91@gmail.com>
2024-06-13 13:13:42 -0400

    add rawbody to stripe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+  const logger = new Logger('Main-payments');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(envs.port);
+  logger.log(`payments running on port ${envs.port}`);
 }
 bootstrap();
