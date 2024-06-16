dd642c1830e2015dbf94f4c2ebceebf568ae8bcd
Eduardopmx <eduardop.29.91@gmail.com>
2024-06-16 13:22:53 -0400

    add microservices nast

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,6 +15,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.nast_servers,
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(envs.port);
   logger.log(`payments running on port ${envs.port}`);
 }
