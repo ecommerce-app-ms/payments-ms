105bc874fb873ef41e11d529b8d61a6989e1ddc7
Eduardopmx <eduardop.29.91@gmail.com>
2024-06-17 06:08:27 -0400

    import nast module in payments module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { NastModule } from 'src/transports/nast.module';
 
 @Module({
+  imports: [NastModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
